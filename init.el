1cf93239a949852d36cb6e5d6a05bd7695928181
Chris Maher and David Dening <pair+cmaher+ddening@pivotallabs.com>
2015-04-21 08:31:00 -0400

    added init.el

---
+++ init.el
@@ -0,0 +1,12 @@
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(load-theme 'alect-dark t)
+(smartparens-global-mode t)
+(show-paren-mode 1)

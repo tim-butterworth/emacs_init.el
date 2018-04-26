eedb26cedf27884dffcfcd620a6a46f8d41b1af5
tim-butterworth <timothy.d.butterworth@gmail.com>
2018-04-26 09:12:08 -0700

    Update init.el
    Adds multi-cursor and turns of ~ backup files.

--- init.el
+++ init.el
@@ -5,8 +5,27 @@
 
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
 (package-initialize)
 
+(fset 'yes-or-no-p 'y-or-n-p)
 (load-theme 'alect-dark t)
 (smartparens-global-mode t)
 (show-paren-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(make-backup-files nil)
+ '(package-selected-packages (quote (multiple-cursors smartparens alect-themes))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(global-set-key (kbd "C-c m c") 'mc/edit-lines)
